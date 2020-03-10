c4304b99e7478d6db780b1eadd2ab50625250271
selmafederl <selma.federl@gmx.at>
2020-03-10 12:22:43 +0100

    Stand 10.03.2020

--- demos/02_Jinja_Demo.py
+++ demos/02_Jinja_Demo.py
@@ -3,18 +3,25 @@ from flask import render_template
 from flask import request
 app = Flask("Hello World")
 
-
-@app.route('/hello', methods=["GET", "POST"])
+@app.route('/hello')
 def hello_world():
+	return render_template('index.html')
+
+@app.route("/test")
+def sternzeichen_seite():
+    return render_template("sternzeichen.html") #hier könnte man eine zweite Seite verlinken, um so auf die nächste Seite zu kommen
+
+@app.route("/formular")
+def formular():
+    return render_template("formular.html")
+
+@app.route('/formular', methods=["GET", "POST"])
+def test():
 	if request.method == "POST":
 		vorname = request.form["vorname"]
 		return "Hallo " + vorname
-	return render_template('index.html')
-
-
-@app.route("/test")
-def test():
-    return render_template("sternzeichen.html") #hier könnte man eine zweite Seite verlinken, um so auf die nächste Seite zu kommen
+	return render_template('formular.html')
+#
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
